1a3f9aa0cdaae272b215aa40c1d68911a60063d8
Sandar <sandar@Sandar-Felicity.local>
2021-08-31 22:44:24 +0200

    - added GTV to multivariate cox model and removed tumor differentiation - added the proportion (%) of dogs alive at 1 and 2 years for OS and PFS

--- r_code/analysis_SIB.R
+++ r_code/analysis_SIB.R
@@ -1,16 +1,14 @@
-#Protocol
+# Protocol: SIB vs regular
 #=============
 
-#Log-rank tests
+#Basic log-rank tests
 #--------
-survdiff(Surv(PFS,Progression_y_n) ~ Protocol , data = dat, rho = 0)
-survdiff(Surv(PFS,Progression_y_n) ~ Protocol , data = dat, rho = 1)
-survdiff(Surv(OS,Dead_y_n) ~ Protocol , data = dat, rho = 0)
-survdiff(Surv(OS,Dead_y_n) ~ Protocol , data = dat, rho = 1)
+survdiff(Surv(PFS,Progression_or_Death_y_n) ~ Protocol , data = dat)
+survdiff(Surv(OS,Dead_y_n) ~ Protocol , data = dat)
 
 #KM curves
 #--------
-KM_P_PFS <- survfit(Surv(PFS,Progression_y_n) ~ Protocol , data = dat)
+KM_P_PFS <- survfit(Surv(PFS,Progression_or_Death_y_n) ~ Protocol , data = dat)
 plot_KM_P_PFS <- ggsurvplot(KM_P_PFS,
            pval = TRUE, conf.int = TRUE,
            risk.table = TRUE, # Add risk table
@@ -38,9 +36,15 @@ ggsurvplot(KM_P_OS,
 
 #Stage
 #=============
-KM_stage_PFS <- survfit(Surv(PFS,Progression_y_n) ~ Stage4_y_n , data = dat)
+#Basic log-rank tests
+#--------
+survdiff(Surv(PFS,Progression_or_Death_y_n) ~ Stage4_y_n , data = dat)
+survdiff(Surv(OS,Dead_y_n) ~ Stage4_y_n , data = dat)
 
-plot_KM_stage_PFS <- ggsurvplot(KM_stage,
+
+KM_stage_PFS <- survfit(Surv(PFS,Progression_or_Death_y_n) ~ Stage4_y_n , data = dat)
+
+plot_KM_stage_PFS <- ggsurvplot(KM_stage_PFS,
                      pval = TRUE, conf.int = TRUE,
                      risk.table = TRUE, # Add risk table
                      risk.table.col = "strata", # Change risk table color by groups
@@ -54,7 +58,7 @@ plot_KM_stage_PFS
 
 KM_stage_OS <- survfit(Surv(OS,Dead_y_n) ~ Stage4_y_n , data = dat)
 
-plot_KM_stage_OS <- ggsurvplot(KM_stage2,
+plot_KM_stage_OS <- ggsurvplot(KM_stage_OS,
                            pval = TRUE, conf.int = TRUE,
                            risk.table = TRUE, # Add risk table
                            risk.table.col = "strata", # Change risk table color by groups
@@ -68,8 +72,26 @@ plot_KM_stage_OS
 
 
 
-#Univariate analysis of prognostic factors
+#Univariate analysis of prog.
 #======================
+#PFS
+p <- coxph(Surv(PFS, Progression_or_Death_y_n) ~  Protocol , data = dat) 
+summary(p)
+age <- coxph(Surv(PFS, Progression_or_Death_y_n) ~ Age + strata(Protocol), data = dat) 
+summary(age)
+weight <- coxph(Surv(PFS, Progression_or_Death_y_n) ~ Weight + strata(Protocol), data = dat) 
+summary(weight)
+stage4 <- coxph(Surv(PFS, Progression_or_Death_y_n) ~ Stage4_y_n + strata(Protocol), data = dat) 
+summary(stage4)
+epistaxis <- coxph(Surv(PFS, Progression_or_Death_y_n) ~ Epistaxis + strata(Protocol), data = dat) 
+summary(epistaxis) #
+tumor_diff <- coxph(Surv(PFS, Progression_or_Death_y_n) ~ Tumor_undifferentiated + strata(Protocol), data = dat) 
+summary(tumor_diff)
+tumor_code <- coxph(Surv(PFS, Progression_or_Death_y_n) ~ TumorCode + strata(Protocol), data = dat) 
+summary(tumor_code)
+tumor_vol <- coxph(Surv(PFS, Progression_or_Death_y_n) ~ GTV + strata(Protocol), data = dat) 
+summary(tumor_vol)
+
 #OS
 p2 <- coxph(Surv(OS,Dead_y_n) ~  Protocol , data = dat) 
 summary(p2)
@@ -85,56 +107,38 @@ tumor_diff2 <- coxph(Surv(OS,Dead_y_n) ~ Tumor_undifferentiated + strata(Protoco
 summary(tumor_diff2)
 tumor_code2 <- coxph(Surv(OS,Dead_y_n) ~ TumorCode + strata(Protocol), data = dat) 
 summary(tumor_code2)
+tumor_vol2 <- coxph(Surv(OS,Dead_y_n) ~ GTV + strata(Protocol), data = dat) 
+summary(tumor_vol2)
 
-#PFS
-p <- coxph(Surv(PFS, Progression_y_n) ~  Protocol , data = dat) 
-summary(p)
-age <- coxph(Surv(PFS, Progression_y_n) ~ Age + strata(Protocol), data = dat) 
-summary(age)
-weight <- coxph(Surv(PFS, Progression_y_n) ~ Weight + strata(Protocol), data = dat) 
-summary(weight)
-stage4 <- coxph(Surv(PFS, Progression_y_n) ~ Stage4_y_n + strata(Protocol), data = dat) 
-summary(stage4)
-epistaxis <- coxph(Surv(PFS, Progression_y_n) ~ Epistaxis + strata(Protocol), data = dat) 
-summary(epistaxis)
-tumor_diff <- coxph(Surv(PFS, Progression_y_n) ~ Tumor_undifferentiated + strata(Protocol), data = dat) 
-summary(tumor_diff)
-tumor_code <- coxph(Surv(PFS, Progression_y_n) ~ TumorCode + strata(Protocol), data = dat) 
-summary(tumor_code)
 
 
 
 #full Cox model
 #------------------
 
-table(dat$Progression_y_n) #34 events
-m_pfs0 <- coxph(Surv(PFS,Progression_y_n) ~ Protocol + 
+table(dat$Progression_or_Death_y_n) #34 events
+m_pfs0 <- coxph(Surv(PFS,Progression_or_Death_y_n) ~ 
+                  Protocol + 
                   Stage4_y_n + 
-                  Tumor_undifferentiated + 
                   Age  + 
                   Weight + 
-                  #TumorCode +
-                  Epistaxis, 
+                  Epistaxis +
+                  GTV, 
                 data = dat) 
 summary(m_pfs0)
 summary(m_pfs0)$coefficients
-m_pfs <- coxph(Surv(PFS,Progression_y_n) ~ Protocol + Stage4_y_n , data = dat) 
-summary(m_pfs)
 
 table(dat$Dead_y_n) #39 events 
 m_os0 <- coxph(Surv(OS,Dead_y_n) ~ Protocol + 
                  Stage4_y_n + 
-                 Tumor_undifferentiated + 
                  Age  + 
                  Weight + 
-            #     TumorCode +
-                 Epistaxis, 
+                 Epistaxis +
+                 GTV, 
                data = dat) 
 summary(m_os0)
 summary(m_os0)$coefficients
 
-m_os <- coxph(Surv(OS,Dead_y_n) ~ Protocol + Stage4_y_n + Weight , data = dat) 
-summary(m_os)
 ggcoxzph(cox.zph(m_os))
 
 
--- r_code/preanalysis_SIB.R
+++ r_code/preanalysis_SIB.R
@@ -5,37 +5,42 @@ library(survminer)
 library(survival)
 library(MASS)
 library(DescTools)
+library(survMisc)
+#data from mail on 30 August:
+dat <- read_excel("data/210830_210721_210526 SinonasaltabelleSIB_for stats_short_2_VMEI_sent to Sandar.xlsx",sheet = "Daten")
 
-dat_og <- read_excel("data/210526 SinonasaltabelleSIB_for stats_short_2_VMEI.xlsx", sheet = "Daten")
-additional_dat <- read_excel("data/210721_210526 SinonasaltabelleSIB_for stats_short_2_VMEI_sent to Sandar.xlsx",sheet = "Daten")
-names(dat_og)
-names(additional_dat)
-dat <- full_join(dat_og, additional_dat)
-str(dat)
-table(dat$TumorCode_simplyfied...9)
+# Tumor code:
 dat$TumorCode <- dat$TumorCode_simplyfied...9
 dat$TumorCode <- ifelse(is.na(dat$TumorCode), "other", dat$TumorCode)
 dat$TumorCode <- ifelse(dat$TumorCode==0, "epithelia", dat$TumorCode)
 dat$TumorCode <- ifelse(dat$TumorCode==1, "mesenchymal", dat$TumorCode)
 
-table(is.na(dat$DateLast_FU))
-table(is.na(dat$FirstRT))
+# calculate follow-up time
 dat$followup <- as.numeric(dat$DateLast_FU-dat$FirstRT)
 
+# configure data types
 dat$Stage4_y_n <- factor(dat$Stage4_y_n)
 dat$Epistaxis <- factor(dat$Epistaxis)
 dat$Tumor_undifferentiated <- factor(dat$Tumor_undifferentiated)
 
-#Events
-dat %>% group_by(Protocol) %>%
-  count(Progression_y_n)
-dat %>% group_by(Protocol) %>%
-  count(Dead_y_n)
+names(dat)
+
+#Events: Progresion and Death
 table(dat$Protocol)
 
-##PFS OS stratified by Protocol
+dat %>% group_by(Protocol) %>%
+  count(Progression_or_Death_y_n)
+dat %>% group_by(Protocol) %>%
+  count(Progression_only_y_n)
+dat %>% group_by(Protocol) %>%
+  count(Dead_y_n)
+
+# Calculating PFS OS stratified by Protocol
+# ------------------
 dat_regular <- dat %>% filter(Protocol==0)
 dat_boost <- dat %>% filter(Protocol==1)
+
+#PFS: regular, boost, all
 MedianCI(dat_regular$PFS,
          conf.level = 0.95,
          na.rm = FALSE,
@@ -52,6 +57,7 @@ MedianCI(dat$PFS,
          method = "exact",
          R = 10000)
 
+#OS: regular, boost, all
 MedianCI(dat_regular$OS,
          conf.level = 0.95,
          na.rm = FALSE,
@@ -68,21 +74,11 @@ MedianCI(dat$OS,
          method = "exact",
          R = 10000)
 
-
-##OS 1 year:
-dat %>%
-  group_by(Protocol) %>%
-  count(OS>365)  %>%
-  mutate(prop = n / sum(n), 
-         lower = lapply(n, prop.test, n = sum(n)), 
-         upper = sapply(lower, function(x) x$conf.int[2]), 
-         lower = sapply(lower, function(x) x$conf.int[1]))
-
-
-
-##PFS OS stratified by Stage
+# Calculating PFS OS stratified by Stage I-III vs Stage IV
+# ------------------
 dat_s1_3 <- dat %>% filter(Stage4_y_n==0)
 dat_s4 <- dat %>% filter(Stage4_y_n==1)
+# PFS
 MedianCI(dat_s1_3$PFS,
          conf.level = 0.95,
          na.rm = FALSE,
@@ -93,7 +89,7 @@ MedianCI(dat_s4$PFS,
          na.rm = FALSE,
          method = "exact",
          R = 10000)
-
+# OS
 MedianCI(dat_s1_3$OS,
          conf.level = 0.95,
          na.rm = FALSE,
@@ -106,14 +102,70 @@ MedianCI(dat_s4$OS,
          R = 10000)
 
 
+# Calculating proportions of dogs alive at 1 year and 2 year (OS)
+# ------------------
+dat %>%
+  group_by(Protocol) %>%
+  count(Dead_y_n)
+##OS 1 year:
+dat %>%
+  group_by(Protocol) %>%
+  count(OS>365.25)  %>%
+  mutate(prop = n / sum(n), 
+         lower = lapply(n, prop.test, n = sum(n)), 
+         upper = sapply(lower, function(x) x$conf.int[2]), 
+         lower = sapply(lower, function(x) x$conf.int[1]))
+#26 dogs alive at the end of 1 year. 
+# 8 survive, 18 died eventually
+##OS 2 year:
+dat %>%
+  group_by(Protocol) %>%
+  count(OS>365.25*2)  %>%
+  mutate(prop = n / sum(n), 
+         lower = lapply(n, prop.test, n = sum(n)), 
+         upper = sapply(lower, function(x) x$conf.int[2]), 
+         lower = sapply(lower, function(x) x$conf.int[1]))
+
+
+
+# Calculating proportions of dogs only progression-free at 1 year and 2 year (PFS)
+#I would also like to know the proportion of dogs free of progression at 1 and at 2 years. This is what some studies looked at in the past. Since for PFS the endpoint was either progression or death, you cannot do the analysis from the table I gave you (=from PFS and the variable progression/death yes/no). I therefore added another column in the excel file in the attachment (highlighted in yellow). <-- Progression_only_y_n
+
+dat %>%
+  group_by(Protocol) %>%
+  count(Progression_only_y_n)
+
+dat <- dat %>%
+  mutate(PFSonly = DateProgr_clin - FirstRT,
+         PFSonly = if_else(is.na(PFSonly),DateLast_FU - FirstRT , PFSonly)) # PFSonly <- either only PFS or max. follow up time
+##only PFS 1 year:
+dat %>%
+  group_by(Protocol) %>%
+  count(PFSonly>365.25)  %>%
+  mutate(prop = n / sum(n), 
+         lower = lapply(n, prop.test, n = sum(n)), 
+         upper = sapply(lower, function(x) x$conf.int[2]), 
+         lower = sapply(lower, function(x) x$conf.int[1]))
+##only PFS 2 year:
+dat %>%
+  group_by(Protocol) %>%
+  count(PFSonly>365.25*2)  %>%
+  mutate(prop = n / sum(n), 
+         lower = lapply(n, prop.test, n = sum(n)), 
+         upper = sapply(lower, function(x) x$conf.int[2]), 
+         lower = sapply(lower, function(x) x$conf.int[1]))
+
+
+
 #relevant parameters for table 1
-
-
+#-------------------
 myVars <- c("Age", 
             "Weight",
             "Sex",
             "Stage", 
-            "OS","PFS", "followup", 
+            "OS",
+            "PFS", 
+            "followup", 
             "TumorCode",
             "Tumor_undifferentiated")
 catVars <- c("Sex", "TumorCode","Tumor_undifferentiated", "Stage")
